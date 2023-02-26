b9996625016ab114f3b68959a31b75dcbf92c4bf
mohdzee-alt <zeeshanmohammed14@gmail.com>
2023-02-26 14:09:07 +0530

    first commit1

---
+++ src/DemoProj/Demo1.java
@@ -0,0 +1,22 @@
+package DemoProj;
+
+public class Demo1 {
+	
+	  public void drum()
+	  {
+		  System.out.println("demo");
+	  }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=10;
+		int j=20;
+		
+		int z=i+j;
+		
+		System.out.println(z);
+	
+
+	}
+
+}
